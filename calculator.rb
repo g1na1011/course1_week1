201f5a0f1427680fc9ab88b89e59136d81cd0721
Gina Zhou <ginayizhou@gmail.com>
2013-09-01 01:05:17 -0700

    updated with string interpolation

--- calculator.rb
+++ calculator.rb
@@ -8,17 +8,21 @@ num1 = gets.chomp
 say "What's the second number?"
 num2 = gets.chomp
 
-say "What operation would you like to perform? 1) add 2) sub 3) multiply 4) divide"
+say "What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide"
 operator = gets.chomp
 
 if operator == "1"
   result = num1.to_i + num2.to_i
+  operation_sign = "+"
 elsif operator == "2"
   result = num1.to_i - num2.to_i
+  operation_sign = "-"
 elsif operator == "3"
   result = num1.to_i * num2.to_i
+  operation_sign = "*"
 else 
   result = num1.to_f / num2.to_f
+  operation_sign = "/"
 end
 
-puts result
+puts "#{num1} #{operation_sign} #{num2} equals to #{result}"
